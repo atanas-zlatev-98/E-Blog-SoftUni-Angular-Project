5d204b4485054cb665b2cc1b6e6adc3d5114315c
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-11-26 23:29:25 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavigationBarComponent],
+  imports: [RouterOutlet, HomeComponent, NavigationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
